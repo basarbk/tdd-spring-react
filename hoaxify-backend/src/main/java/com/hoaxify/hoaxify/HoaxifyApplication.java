deccaf7eac87380eb741e1c2c9420044388bc916
Basar Buyukkahraman <basarbk@yahoo.com>
2019-07-16 21:56:41 +0300

    0201-created-backend-project

---
+++ hoaxify-backend/src/main/java/com/hoaxify/hoaxify/HoaxifyApplication.java
@@ -0,0 +1,13 @@
+package com.hoaxify.hoaxify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxifyApplication.class, args);
+	}
+
+}
