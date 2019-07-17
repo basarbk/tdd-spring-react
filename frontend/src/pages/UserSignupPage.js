ee3f189980ebc712028beca1d6c2cdd1b7afea95
Basar Buyukkahraman <basarbk@yahoo.com>
2019-07-18 00:20:26 +0300

    0306-user-signup-page-first-test

---
+++ frontend/src/pages/UserSignupPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export class UserSignupPage extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Sign Up</h1>
+      </div>
+    );
+  }
+}
+
+export default UserSignupPage;
---
+++ frontend/src/pages/UserSignupPage.spec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { UserSignupPage } from './UserSignupPage';
+
+describe('UserSignupPage', () => {
+  describe('Layout', () => {
+    it('has header of Sign Up', () => {
+      const { container } = render(<UserSignupPage />);
+      const header = container.querySelector('h1');
+      expect(header).toHaveTextContent('Sign Up');
+    });
+  });
+});
