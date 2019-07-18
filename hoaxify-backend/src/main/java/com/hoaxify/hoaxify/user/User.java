47c21db14b74668040b03cb82d174ebfd9e6e8ed
Basar Buyukkahraman <basarbk@yahoo.com>
2019-07-18 23:23:45 +0300

    0404-error-messages-internationalization

--- hoaxify-backend/src/main/java/com/hoaxify/hoaxify/user/User.java
+++ hoaxify-backend/src/main/java/com/hoaxify/hoaxify/user/User.java
@@ -17,7 +17,7 @@ public class User {
 	@GeneratedValue
 	private long id;
 	
-	@NotNull
+	@NotNull(message = "{hoaxify.constraints.username.NotNull.message}")
 	@Size(min = 4, max=255)
 	private String username;
 	
@@ -27,7 +27,7 @@ public class User {
 	
 	@NotNull
 	@Size(min = 8, max=255)
-	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$")
+	@Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message="{hoaxify.constraints.password.Pattern.message}")
 	private String password;
 
 }
--- hoaxify-backend/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
+++ hoaxify-backend/src/test/java/com/hoaxify/hoaxify/UserControllerTest.java
@@ -3,6 +3,7 @@ package com.hoaxify.hoaxify;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -183,6 +184,42 @@ public class UserControllerTest {
 		User user = new User();
 		ResponseEntity<ApiError> response = postSignup(user, ApiError.class);
 		assertThat(response.getBody().getValidationErrors().size()).isEqualTo(3);
+	}
+	
+	@Test
+	public void postUser_whenUserHasNullUsername_receiveMessageOfNullErrorForUsername() {
+		User user = createValidUser();
+		user.setUsername(null);
+		ResponseEntity<ApiError> response = postSignup(user, ApiError.class);
+		Map<String, String> validationErrors = response.getBody().getValidationErrors();
+		assertThat(validationErrors.get("username")).isEqualTo("Username cannot be null");
+	}
+	
+	@Test
+	public void postUser_whenUserHasNullPassword_receiveGenericMessageOfNullError() {
+		User user = createValidUser();
+		user.setPassword(null);
+		ResponseEntity<ApiError> response = postSignup(user, ApiError.class);
+		Map<String, String> validationErrors = response.getBody().getValidationErrors();
+		assertThat(validationErrors.get("password")).isEqualTo("Cannot be null");
+	}
+	
+	@Test
+	public void postUser_whenUserHasInvalidLengthUsername_receiveGenericMessageOfSizeError() {
+		User user = createValidUser();
+		user.setUsername("abc");
+		ResponseEntity<ApiError> response = postSignup(user, ApiError.class);
+		Map<String, String> validationErrors = response.getBody().getValidationErrors();
+		assertThat(validationErrors.get("username")).isEqualTo("It must have minimum 4 and maximum 255 characters");
+	}
+	
+	@Test
+	public void postUser_whenUserHasInvalidPasswordPattern_receiveMessageOfPasswordPatternError() {
+		User user = createValidUser();
+		user.setPassword("alllowercase");
+		ResponseEntity<ApiError> response = postSignup(user, ApiError.class);
+		Map<String, String> validationErrors = response.getBody().getValidationErrors();
+		assertThat(validationErrors.get("password")).isEqualTo("Password must have at least one uppercase, one lowercase letter and one number");
 	}
 	
 	public <T> ResponseEntity<T> postSignup(Object request, Class<T> response){
