2c1c02326b5a42c9fefc34237dbf8f6ee51cc0c0
Basar Buyukkahraman <basarbk@yahoo.com>
2019-07-18 16:19:15 +0300

    0310-styling

--- frontend/src/pages/UserSignupPage.js
+++ frontend/src/pages/UserSignupPage.js
@@ -39,40 +39,50 @@ export class UserSignupPage extends React.Component {
 
   render() {
     return (
-      <div>
-        <h1>Sign Up</h1>
-        <div>
+      <div className="container">
+        <h1 className="text-center">Sign Up</h1>
+        <div className="col-12 mb-3">
+          <label>Display Name</label>
           <input
+            className="form-control"
             placeholder="Your display name"
             value={this.state.displayName}
             onChange={this.onChangeDisplayName}
           />
         </div>
-        <div>
+        <div className="col-12 mb-3">
+          <label>Username</label>
           <input
+            className="form-control"
             placeholder="Your username"
             value={this.state.username}
             onChange={this.onChangeUsername}
           />
         </div>
-        <div>
+        <div className="col-12 mb-3">
+          <label>Password</label>
           <input
+            className="form-control"
             placeholder="Your password"
             type="password"
             value={this.state.password}
             onChange={this.onChangePassword}
           />
         </div>
-        <div>
+        <div className="col-12 mb-3">
+          <label>Password Repeat</label>
           <input
+            className="form-control"
             placeholder="Repeat your password"
             type="password"
             value={this.state.passwordRepeat}
             onChange={this.onChangePasswordRepeat}
           />
         </div>
-        <div>
-          <button onClick={this.onClickSignup}>Sign Up</button>
+        <div className="text-center">
+          <button className="btn btn-primary" onClick={this.onClickSignup}>
+            Sign Up
+          </button>
         </div>
       </div>
     );
