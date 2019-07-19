f9c2f10496a73fd328bf98de8c4d122dab073ddc
Basar Buyukkahraman <basarbk@yahoo.com>
2019-07-19 23:36:51 +0300

    0509-click-handling

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -4,24 +4,47 @@ import Input from '../components/Input';
 export class LoginPage extends React.Component {
   state = {
     username: '',
-    password: ''
+    password: '',
+    apiError: undefined
   };
 
   onChangeUsername = (event) => {
     const value = event.target.value;
     this.setState({
-      username: value
+      username: value,
+      apiError: undefined
     });
   };
 
   onChangePassword = (event) => {
     const value = event.target.value;
     this.setState({
-      password: value
+      password: value,
+      apiError: undefined
+    });
+  };
+
+  onClickLogin = () => {
+    const body = {
+      username: this.state.username,
+      password: this.state.password
+    };
+    this.props.actions.postLogin(body).catch((error) => {
+      if (error.response) {
+        this.setState({ apiError: error.response.data.message });
+      }
     });
   };
 
   render() {
+    let disableSubmit = false;
+    if (this.state.username === '') {
+      disableSubmit = true;
+    }
+    if (this.state.password === '') {
+      disableSubmit = true;
+    }
+
     return (
       <div className="container">
         <h1 className="text-center">Login</h1>
@@ -42,12 +65,29 @@ export class LoginPage extends React.Component {
             onChange={this.onChangePassword}
           />
         </div>
+        {this.state.apiError && (
+          <div className="col-12 mb-3">
+            <div className="alert alert-danger">{this.state.apiError}</div>
+          </div>
+        )}
         <div className="text-center">
-          <button className="btn btn-primary">Login</button>
+          <button
+            className="btn btn-primary"
+            onClick={this.onClickLogin}
+            disabled={disableSubmit}
+          >
+            Login
+          </button>
         </div>
       </div>
     );
   }
 }
+
+LoginPage.defaultProps = {
+  actions: {
+    postLogin: () => new Promise((resolve, reject) => resolve({}))
+  }
+};
 
 export default LoginPage;
--- frontend/src/pages/LoginPage.spec.js
+++ frontend/src/pages/LoginPage.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, waitForElement } from '@testing-library/react';
 import { LoginPage } from './LoginPage';
 
 describe('LoginPage', () => {
@@ -41,6 +41,23 @@ describe('LoginPage', () => {
         }
       };
     };
+
+    let usernameInput, passwordInput, button;
+
+    const setupForSubmit = (props) => {
+      const rendered = render(<LoginPage {...props} />);
+
+      const { container, queryByPlaceholderText } = rendered;
+
+      usernameInput = queryByPlaceholderText('Your username');
+      fireEvent.change(usernameInput, changeEvent('my-user-name'));
+      passwordInput = queryByPlaceholderText('Your password');
+      fireEvent.change(passwordInput, changeEvent('P4ssword'));
+      button = container.querySelector('button');
+
+      return rendered;
+    };
+
     it('sets the username value into state', () => {
       const { queryByPlaceholderText } = render(<LoginPage />);
       const usernameInput = queryByPlaceholderText('Your username');
@@ -52,6 +69,102 @@ describe('LoginPage', () => {
       const passwordInput = queryByPlaceholderText('Your password');
       fireEvent.change(passwordInput, changeEvent('P4ssword'));
       expect(passwordInput).toHaveValue('P4ssword');
+    });
+    it('calls postLogin when the actions are provided in props and input fields have value', () => {
+      const actions = {
+        postLogin: jest.fn().mockResolvedValue({})
+      };
+      setupForSubmit({ actions });
+      fireEvent.click(button);
+      expect(actions.postLogin).toHaveBeenCalledTimes(1);
+    });
+    it('does not throw exception when clicking the button when actions not provided in props', () => {
+      setupForSubmit();
+      expect(() => fireEvent.click(button)).not.toThrow();
+    });
+
+    it('calls postLogin with credentials in body', () => {
+      const actions = {
+        postLogin: jest.fn().mockResolvedValue({})
+      };
+      setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      const expectedUserObject = {
+        username: 'my-user-name',
+        password: 'P4ssword'
+      };
+
+      expect(actions.postLogin).toHaveBeenCalledWith(expectedUserObject);
+    });
+
+    it('enables the button when username and password is not empty', () => {
+      setupForSubmit();
+      expect(button).not.toBeDisabled();
+    });
+    it('disables the button when username is empty', () => {
+      setupForSubmit();
+      fireEvent.change(usernameInput, changeEvent(''));
+      expect(button).toBeDisabled();
+    });
+    it('disables the button when password is empty', () => {
+      setupForSubmit();
+      fireEvent.change(passwordInput, changeEvent(''));
+      expect(button).toBeDisabled();
+    });
+    it('displays alert when login fails', async () => {
+      const actions = {
+        postLogin: jest.fn().mockRejectedValue({
+          response: {
+            data: {
+              message: 'Login failed'
+            }
+          }
+        })
+      };
+      const { queryByText } = setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      const alert = await waitForElement(() => queryByText('Login failed'));
+      expect(alert).toBeInTheDocument();
+    });
+    it('clears alert when user changes username', async () => {
+      const actions = {
+        postLogin: jest.fn().mockRejectedValue({
+          response: {
+            data: {
+              message: 'Login failed'
+            }
+          }
+        })
+      };
+      const { queryByText } = setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      await waitForElement(() => queryByText('Login failed'));
+      fireEvent.change(usernameInput, changeEvent('updated-username'));
+
+      const alert = queryByText('Login failed');
+      expect(alert).not.toBeInTheDocument();
+    });
+    it('clears alert when user changes password', async () => {
+      const actions = {
+        postLogin: jest.fn().mockRejectedValue({
+          response: {
+            data: {
+              message: 'Login failed'
+            }
+          }
+        })
+      };
+      const { queryByText } = setupForSubmit({ actions });
+      fireEvent.click(button);
+
+      await waitForElement(() => queryByText('Login failed'));
+      fireEvent.change(passwordInput, changeEvent('updated-P4ssword'));
+
+      const alert = queryByText('Login failed');
+      expect(alert).not.toBeInTheDocument();
     });
   });
 });
