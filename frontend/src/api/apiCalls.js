8210e3bd5c0690640e2d049b275606df7e25f70a
Basar Buyukkahraman <basarbk@yahoo.com>
2019-07-26 22:19:39 +0300

    0809-user-list-api

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -17,3 +17,8 @@ export const setAuthorizationHeader = ({ username, password, isLoggedIn }) => {
     delete axios.defaults.headers.common['Authorization'];
   }
 };
+
+export const listUsers = (param = { page: 0, size: 3 }) => {
+  const path = `/api/1.0/users?page=${param.page || 0}&size=${param.size || 3}`;
+  return axios.get(path);
+};
--- frontend/src/api/apiCalls.spec.js
+++ frontend/src/api/apiCalls.spec.js
@@ -20,4 +20,30 @@ describe('apiCalls', () => {
       expect(path).toBe('/api/1.0/login');
     });
   });
+  describe('listUser', () => {
+    it('calls /api/1.0/users?page=0&size=3 when no param provided for listUsers', () => {
+      const mockListUsers = jest.fn();
+      axios.get = mockListUsers;
+      apiCalls.listUsers();
+      expect(mockListUsers).toBeCalledWith('/api/1.0/users?page=0&size=3');
+    });
+    it('calls /api/1.0/users?page=5&size=10 when corresponding params provided for listUsers', () => {
+      const mockListUsers = jest.fn();
+      axios.get = mockListUsers;
+      apiCalls.listUsers({ page: 5, size: 10 });
+      expect(mockListUsers).toBeCalledWith('/api/1.0/users?page=5&size=10');
+    });
+    it('calls /api/1.0/users?page=5&size=3 when only page param provided for listUsers', () => {
+      const mockListUsers = jest.fn();
+      axios.get = mockListUsers;
+      apiCalls.listUsers({ page: 5 });
+      expect(mockListUsers).toBeCalledWith('/api/1.0/users?page=5&size=3');
+    });
+    it('calls /api/1.0/users?page=0&size=5 when only size param provided for listUsers', () => {
+      const mockListUsers = jest.fn();
+      axios.get = mockListUsers;
+      apiCalls.listUsers({ size: 5 });
+      expect(mockListUsers).toBeCalledWith('/api/1.0/users?page=0&size=5');
+    });
+  });
 });
