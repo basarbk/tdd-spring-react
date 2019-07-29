842f41d7330160bdd2050022c1d91088118659ec
Basar Buyukkahraman <basarbk@yahoo.com>
2019-07-29 22:37:59 +0300

    0905-load-user-failure

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -3,19 +3,37 @@ import * as apiCalls from '../api/apiCalls';
 
 class UserPage extends React.Component {
   state = {
-    user: undefined
+    user: undefined,
+    userNotFound: false
   };
   componentDidMount() {
     const username = this.props.match.params.username;
     if (!username) {
       return;
     }
-    apiCalls.getUser(username).then((response) => {
-      this.setState({ user: response.data });
-    });
+    apiCalls
+      .getUser(username)
+      .then((response) => {
+        this.setState({ user: response.data });
+      })
+      .catch((error) => {
+        this.setState({
+          userNotFound: true
+        });
+      });
   }
 
   render() {
+    if (this.state.userNotFound) {
+      return (
+        <div className="alert alert-danger text-center">
+          <div className="alert-heading">
+            <i className="fas fa-exclamation-triangle fa-3x" />
+          </div>
+          <h5>User not found</h5>
+        </div>
+      );
+    }
     return (
       <div data-testid="userpage">
         {this.state.user && (
--- frontend/src/pages/UserPage.spec.js
+++ frontend/src/pages/UserPage.spec.js
@@ -11,35 +11,54 @@ const mockSuccessGetUser = {
     image: 'profile1.png'
   }
 };
+
+const mockFailGetUser = {
+  response: {
+    data: {
+      message: 'User not found'
+    }
+  }
+};
+
 const match = {
   params: {
     username: 'user1'
   }
 };
 
+const setup = (props) => {
+  return render(<UserPage {...props} />);
+};
+
 describe('UserPage', () => {
   describe('Layout', () => {
     it('has root page div', () => {
-      const { queryByTestId } = render(<UserPage />);
+      const { queryByTestId } = setup();
       const userPageDiv = queryByTestId('userpage');
       expect(userPageDiv).toBeInTheDocument();
     });
     it('displays the displayName@username when user data loaded', async () => {
       apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
-      const { queryByText } = render(<UserPage match={match} />);
+      const { queryByText } = setup({ match });
       const text = await waitForElement(() => queryByText('display1@user1'));
       expect(text).toBeInTheDocument();
+    });
+    it('displays not found alert when user not found', async () => {
+      apiCalls.getUser = jest.fn().mockRejectedValue(mockFailGetUser);
+      const { queryByText } = setup({ match });
+      const alert = await waitForElement(() => queryByText('User not found'));
+      expect(alert).toBeInTheDocument();
     });
   });
   describe('Lifecycle', () => {
     it('calls getUser when it is rendered', () => {
       apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
-      render(<UserPage match={match} />);
+      setup({ match });
       expect(apiCalls.getUser).toHaveBeenCalledTimes(1);
     });
     it('calls getUser for user1 when it is rendered with user1 in match', () => {
       apiCalls.getUser = jest.fn().mockResolvedValue(mockSuccessGetUser);
-      render(<UserPage match={match} />);
+      setup({ match });
       expect(apiCalls.getUser).toHaveBeenCalledWith('user1');
     });
   });
