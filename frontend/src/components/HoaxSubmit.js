f388d04890aac6e0cde25b8592206e57430f47ef
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-06 23:38:50 +0300

    1410-user-interactions

--- frontend/src/components/HoaxSubmit.js
+++ frontend/src/components/HoaxSubmit.js
@@ -3,6 +3,22 @@ import ProfileImageWithDefault from './ProfileImageWithDefault';
 import { connect } from 'react-redux';
 
 class HoaxSubmit extends Component {
+  state = {
+    focused: false
+  };
+
+  onFocus = () => {
+    this.setState({
+      focused: true
+    });
+  };
+
+  onClickCancel = () => {
+    this.setState({
+      focused: false
+    });
+  };
+
   render() {
     return (
       <div className="card d-flex flex-row p-1">
@@ -13,7 +29,22 @@ class HoaxSubmit extends Component {
           image={this.props.loggedInUser.image}
         />
         <div className="flex-fill">
-          <textarea className="form-control w-100" rows={1} />
+          <textarea
+            className="form-control w-100"
+            rows={this.state.focused ? 3 : 1}
+            onFocus={this.onFocus}
+          />
+          {this.state.focused && (
+            <div className="text-right mt-1">
+              <button className="btn btn-success">Hoaxify</button>
+              <button
+                className="btn btn-light ml-1"
+                onClick={this.onClickCancel}
+              >
+                <i className="fas fa-times"></i> Cancel
+              </button>
+            </div>
+          )}
         </div>
       </div>
     );
--- frontend/src/components/HoaxSubmit.spec.js
+++ frontend/src/components/HoaxSubmit.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import HoaxSubmit from './HoaxSubmit';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
@@ -46,6 +46,46 @@ describe('HoaxSubmit', () => {
       const { container } = setup();
       const image = container.querySelector('img');
       expect(image.src).toContain('/images/profile/' + defaultState.image);
+    });
+  });
+  describe('Interactions', () => {
+    it('displays 3 rows when focused to textarea', () => {
+      const { container } = setup();
+      const textArea = container.querySelector('textarea');
+      fireEvent.focus(textArea);
+      expect(textArea.rows).toBe(3);
+    });
+    it('displays hoaxify button when focused to textarea', () => {
+      const { container, queryByText } = setup();
+      const textArea = container.querySelector('textarea');
+      fireEvent.focus(textArea);
+      const hoaxifyButton = queryByText('Hoaxify');
+      expect(hoaxifyButton).toBeInTheDocument();
+    });
+    it('displays Cancel button when focused to textarea', () => {
+      const { container, queryByText } = setup();
+      const textArea = container.querySelector('textarea');
+      fireEvent.focus(textArea);
+      const cancelButton = queryByText('Cancel');
+      expect(cancelButton).toBeInTheDocument();
+    });
+    it('does not display Hoaxify button when not focused to textarea', () => {
+      const { queryByText } = setup();
+      const hoaxifyButton = queryByText('Hoaxify');
+      expect(hoaxifyButton).not.toBeInTheDocument();
+    });
+    it('does not display Cancel button when not focused to textarea', () => {
+      const { queryByText } = setup();
+      const cancelButton = queryByText('Cancel');
+      expect(cancelButton).not.toBeInTheDocument();
+    });
+    it('returns back to unfocused state after clicking the cancel', () => {
+      const { container, queryByText } = setup();
+      const textArea = container.querySelector('textarea');
+      fireEvent.focus(textArea);
+      const cancelButton = queryByText('Cancel');
+      fireEvent.click(cancelButton);
+      expect(queryByText('Cancel')).not.toBeInTheDocument();
     });
   });
 });
