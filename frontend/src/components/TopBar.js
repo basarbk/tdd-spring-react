d973432575d3c7e6e348f977c2e98431f02422c6
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-06 23:05:53 +0300

    1302-topbar-with-user-info

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import logo from '../assets/hoaxify-logo.png';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import ProfileImageWithDefault from './ProfileImageWithDefault';
 
 class TopBar extends React.Component {
   onClickLogout = () => {
@@ -29,6 +30,15 @@ class TopBar extends React.Component {
     if (this.props.user.isLoggedIn) {
       links = (
         <ul className="nav navbar-nav ml-auto">
+          <li className="nav-item nav-link">
+            <ProfileImageWithDefault
+              className="rounded-circle"
+              width="32"
+              height="32"
+              image={this.props.user.image}
+            />
+            {this.props.user.displayName}
+          </li>
           <li
             className="nav-item nav-link"
             onClick={this.onClickLogout}
--- frontend/src/components/TopBar.spec.js
+++ frontend/src/components/TopBar.spec.js
@@ -69,6 +69,17 @@ describe('TopBar', () => {
       const profileLink = queryByText('My Profile');
       expect(profileLink.getAttribute('href')).toBe('/user1');
     });
+    it('displays the displayName when user logged in', () => {
+      const { queryByText } = setup(loggedInState);
+      const displayName = queryByText('display1');
+      expect(displayName).toBeInTheDocument();
+    });
+    it('displays users image when user logged in', () => {
+      const { container } = setup(loggedInState);
+      const images = container.querySelectorAll('img');
+      const userImage = images[1];
+      expect(userImage.src).toContain('/images/profile/' + loggedInState.image);
+    });
   });
   describe('Interactions', () => {
     it('displays the login and signup links when user clicks logout', () => {
