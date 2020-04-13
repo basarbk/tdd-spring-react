0d75c0cd93b8584001834ccdf508a387e5d1cfb0
Basar Buyukkahraman <basarbk@yahoo.com>
2020-04-13 23:58:17 +0300

    2008-HoaxFeed

--- frontend/src/components/HoaxFeed.js
+++ frontend/src/components/HoaxFeed.js
@@ -1,167 +1,163 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import * as apiCalls from '../api/apiCalls';
 import Spinner from './Spinner';
 import HoaxView from './HoaxView';
 import Modal from './Modal';
-class HoaxFeed extends Component {
-  state = {
-    page: {
-      content: []
-    },
-    isLoadingHoaxes: false,
-    newHoaxCount: 0,
-    isLoadingOldHoaxes: false,
-    isLoadingNewHoaxes: false,
-    isDeletingHoax: false
-  };
 
-  componentDidMount() {
-    this.setState({ isLoadingHoaxes: true });
-    apiCalls.loadHoaxes(this.props.user).then((response) => {
-      this.setState({ page: response.data, isLoadingHoaxes: false }, () => {
-        this.counter = setInterval(this.checkCount, 3000);
+const HoaxFeed = (props) => {
+  const [page, setPage] = useState({ content: [] });
+  const [isLoadingHoaxes, setLoadingHoaxes] = useState(false);
+  const [isLoadingOldHoaxes, setLoadingOldHoaxes] = useState(false);
+  const [isLoadingNewHoaxes, setLoadingNewHoaxes] = useState(false);
+  const [isDeletingHoax, setDeletingHoax] = useState(false);
+  const [newHoaxCount, setNewHoaxCount] = useState(0);
+  const [hoaxToBeDeleted, setHoaxToBeDeleted] = useState();
+
+  useEffect(() => {
+    const loadHoaxes = () => {
+      setLoadingHoaxes(true);
+      apiCalls.loadHoaxes(props.user).then((response) => {
+        setLoadingHoaxes(false);
+        setPage(response.data);
       });
-    });
-  }
+    };
+    loadHoaxes();
+  }, [props.user]);
 
-  componentWillUnmount() {
-    clearInterval(this.counter);
-  }
+  useEffect(() => {
+    const checkCount = () => {
+      const hoaxes = page.content;
+      let topHoaxId = 0;
+      if (hoaxes.length > 0) {
+        topHoaxId = hoaxes[0].id;
+      }
+      apiCalls.loadNewHoaxCount(topHoaxId, props.user).then((response) => {
+        setNewHoaxCount(response.data.count);
+      });
+    };
+    const counter = setInterval(checkCount, 3000);
+    return function cleanup() {
+      clearInterval(counter);
+    };
+  }, [props.user, page.content]);
 
-  checkCount = () => {
-    const hoaxes = this.state.page.content;
-    let topHoaxId = 0;
-    if (hoaxes.length > 0) {
-      topHoaxId = hoaxes[0].id;
-    }
-    apiCalls.loadNewHoaxCount(topHoaxId, this.props.user).then((response) => {
-      this.setState({ newHoaxCount: response.data.count });
-    });
-  };
-
-  onClickLoadMore = () => {
-    const hoaxes = this.state.page.content;
+  const onClickLoadMore = () => {
+    const hoaxes = page.content;
     if (hoaxes.length === 0) {
       return;
     }
     const hoaxAtBottom = hoaxes[hoaxes.length - 1];
-    this.setState({ isLoadingOldHoaxes: true });
+    setLoadingOldHoaxes(true);
     apiCalls
-      .loadOldHoaxes(hoaxAtBottom.id, this.props.user)
+      .loadOldHoaxes(hoaxAtBottom.id, props.user)
       .then((response) => {
-        const page = { ...this.state.page };
-        page.content = [...page.content, ...response.data.content];
-        page.last = response.data.last;
-        this.setState({ page, isLoadingOldHoaxes: false });
+        setPage((previousPage) => ({
+          ...previousPage,
+          last: response.data.last,
+          content: [...previousPage.content, ...response.data.content],
+        }));
+        setLoadingOldHoaxes(false);
       })
       .catch((error) => {
-        this.setState({ isLoadingOldHoaxes: false });
+        setLoadingOldHoaxes(false);
       });
   };
 
-  onClickLoadNew = () => {
-    const hoaxes = this.state.page.content;
+  const onClickLoadNew = () => {
+    const hoaxes = page.content;
     let topHoaxId = 0;
     if (hoaxes.length > 0) {
       topHoaxId = hoaxes[0].id;
     }
-    this.setState({ isLoadingNewHoaxes: true });
+    setLoadingNewHoaxes(true);
     apiCalls
-      .loadNewHoaxes(topHoaxId, this.props.user)
+      .loadNewHoaxes(topHoaxId, props.user)
       .then((response) => {
-        const page = { ...this.state.page };
-        page.content = [...response.data, ...page.content];
-        this.setState({ page, newHoaxCount: 0, isLoadingNewHoaxes: false });
+        setPage((previousPage) => ({
+          ...previousPage,
+          content: [...response.data, ...previousPage.content],
+        }));
+        setLoadingNewHoaxes(false);
+        setNewHoaxCount(0);
       })
       .catch((error) => {
-        this.setState({ isLoadingNewHoaxes: false });
+        setLoadingNewHoaxes(false);
       });
   };
 
-  onClickDeleteHoax = (hoax) => {
-    this.setState({ hoaxToBeDeleted: hoax });
-  };
-
-  onClickModalCancel = () => {
-    this.setState({ hoaxToBeDeleted: undefined });
-  };
-
-  onClickModalOk = () => {
-    this.setState({ isDeletingHoax: true });
-    apiCalls.deleteHoax(this.state.hoaxToBeDeleted.id).then((response) => {
-      const page = { ...this.state.page };
-      page.content = page.content.filter(
-        (hoax) => hoax.id !== this.state.hoaxToBeDeleted.id
-      );
-      this.setState({
-        hoaxToBeDeleted: undefined,
-        page,
-        isDeletingHoax: false
-      });
+  const onClickModalOk = () => {
+    setDeletingHoax(true);
+    apiCalls.deleteHoax(hoaxToBeDeleted.id).then((response) => {
+      setPage((previousPage) => ({
+        ...previousPage,
+        content: previousPage.content.filter(
+          (hoax) => hoax.id !== hoaxToBeDeleted.id
+        ),
+      }));
+      setDeletingHoax(false);
+      setHoaxToBeDeleted();
     });
   };
 
-  render() {
-    if (this.state.isLoadingHoaxes) {
-      return <Spinner />;
-    }
-    if (this.state.page.content.length === 0 && this.state.newHoaxCount === 0) {
-      return (
-        <div className="card card-header text-center">There are no hoaxes</div>
-      );
-    }
-    const newHoaxCountMessage =
-      this.state.newHoaxCount === 1
-        ? 'There is 1 new hoax'
-        : `There are ${this.state.newHoaxCount} new hoaxes`;
+  if (isLoadingHoaxes) {
+    return <Spinner />;
+  }
+  if (page.content.length === 0 && newHoaxCount === 0) {
     return (
-      <div>
-        {this.state.newHoaxCount > 0 && (
-          <div
-            className="card card-header text-center"
-            onClick={!this.state.isLoadingNewHoaxes && this.onClickLoadNew}
-            style={{
-              cursor: this.state.isLoadingNewHoaxes ? 'not-allowed' : 'pointer'
-            }}
-          >
-            {this.state.isLoadingNewHoaxes ? <Spinner /> : newHoaxCountMessage}
-          </div>
-        )}
-        {this.state.page.content.map((hoax) => {
-          return (
-            <HoaxView
-              key={hoax.id}
-              hoax={hoax}
-              onClickDelete={() => this.onClickDeleteHoax(hoax)}
-            />
-          );
-        })}
-        {this.state.page.last === false && (
-          <div
-            className="card card-header text-center"
-            onClick={!this.state.isLoadingOldHoaxes && this.onClickLoadMore}
-            style={{
-              cursor: this.state.isLoadingOldHoaxes ? 'not-allowed' : 'pointer'
-            }}
-          >
-            {this.state.isLoadingOldHoaxes ? <Spinner /> : 'Load More'}
-          </div>
-        )}
-        <Modal
-          visible={this.state.hoaxToBeDeleted && true}
-          onClickCancel={this.onClickModalCancel}
-          body={
-            this.state.hoaxToBeDeleted &&
-            `Are you sure to delete '${this.state.hoaxToBeDeleted.content}'?`
-          }
-          title="Delete!"
-          okButton="Delete Hoax"
-          onClickOk={this.onClickModalOk}
-          pendingApiCall={this.state.isDeletingHoax}
-        />
-      </div>
+      <div className="card card-header text-center">There are no hoaxes</div>
     );
   }
-}
+  const newHoaxCountMessage =
+    newHoaxCount === 1
+      ? 'There is 1 new hoax'
+      : `There are ${newHoaxCount} new hoaxes`;
+  return (
+    <div>
+      {newHoaxCount > 0 && (
+        <div
+          className="card card-header text-center"
+          onClick={!isLoadingNewHoaxes && onClickLoadNew}
+          style={{
+            cursor: isLoadingNewHoaxes ? 'not-allowed' : 'pointer',
+          }}
+        >
+          {isLoadingNewHoaxes ? <Spinner /> : newHoaxCountMessage}
+        </div>
+      )}
+      {page.content.map((hoax) => {
+        return (
+          <HoaxView
+            key={hoax.id}
+            hoax={hoax}
+            onClickDelete={() => setHoaxToBeDeleted(hoax)}
+          />
+        );
+      })}
+      {page.last === false && (
+        <div
+          className="card card-header text-center"
+          onClick={!isLoadingOldHoaxes && onClickLoadMore}
+          style={{
+            cursor: isLoadingOldHoaxes ? 'not-allowed' : 'pointer',
+          }}
+        >
+          {isLoadingOldHoaxes ? <Spinner /> : 'Load More'}
+        </div>
+      )}
+      <Modal
+        visible={hoaxToBeDeleted && true}
+        onClickCancel={() => setHoaxToBeDeleted()}
+        body={
+          hoaxToBeDeleted &&
+          `Are you sure to delete '${hoaxToBeDeleted.content}'?`
+        }
+        title="Delete!"
+        okButton="Delete Hoax"
+        onClickOk={onClickModalOk}
+        pendingApiCall={isDeletingHoax}
+      />
+    </div>
+  );
+};
 
 export default HoaxFeed;
